06d8c594ce85ce750043f5dbc12a5e917cd13807
Falcon <falcon@zigtag.dev>
2021-09-05 17:51:16 -0700

    wip: ace 2 support

--- src/instruments/buildSrc/browserBuild.mjs
+++ src/instruments/buildSrc/browserBuild.mjs
@@ -1,39 +1,29 @@
 import fs from 'fs';
 import { join } from 'path';
-import serve from 'rollup-plugin-serve';
-import liveReload from 'rollup-plugin-livereload';
 import { baseCompile } from './plugins.mjs';
 import { Directories } from './directories.mjs';
+import { getInputs } from './igniter/tasks.mjs';
 
 process.chdir(Directories.src);
 
-function getInputs() {
-    const baseInstruments = fs.readdirSync(join(Directories.instruments, 'src'), { withFileTypes: true })
-        .filter((d) => d.isDirectory() && fs.existsSync(join(Directories.instruments, 'src', d.name, 'config.json')));
-
-    return [
-        ...baseInstruments.map(({ name }) => ({
-            path: name,
-            name,
-        })),
-    ];
+if (!fs.existsSync(join(Directories.instruments, 'devServer/bundles'))) {
+    fs.mkdirSync(join(Directories.instruments, 'devServer/bundles'), {recursive: true});
 }
 
 const builds = getInputs()
-    .map(({ path }) => {
+    .map(({ path, name }) => {
         const config = JSON.parse(fs.readFileSync(join(Directories.instruments, 'src', path, 'config.json')));
 
         return {
             watch: true,
+            name,
             input: join(Directories.instruments, 'src', path, config.index),
             output: {
                 file: join(Directories.instruments, 'devServer/bundles', path, 'bundle.js'),
                 format: 'iife',
             },
             plugins: [
-                ...baseCompile(path, path),
-                serve(`${Directories.instruments}/devServer`),
-                liveReload(),
+                ...baseCompile(name, path),
             ],
         };
     });
