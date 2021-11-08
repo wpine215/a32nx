82934dcc4d1cb73d5507cc2ea3d0e08157ff9c0a
BlueberryKing <22713769+BlueberryKing@users.noreply.github.com>
2021-11-08 22:03:55 +0100

    Add proper climb thrust table

--- src/fmgc/src/guidance/vnav/climb/ClimbPathBuilder.ts
+++ src/fmgc/src/guidance/vnav/climb/ClimbPathBuilder.ts
@@ -2,15 +2,16 @@ import { Geometry } from '@fmgc/guidance/Geometry';
 import { Predictions, StepResults } from '../Predictions';
 import { ClimbProfileBuilderResult } from './ClimbProfileBuilderResult';
 import { Common, FlapConf } from '../common';
-import { FlightPlanManager } from '@fmgc/wtsdk';
 import { Fmgc } from '@fmgc/guidance/GuidanceController';
 import { EngineModel } from '../EngineModel';
 import { TFLeg } from '@fmgc/guidance/lnav/legs/TF';
 import { RFLeg } from '@fmgc/guidance/lnav/legs/RF';
+import { AltitudeConstraint } from '@fmgc/guidance/lnav/legs';
 
 interface VerticalCheckpoint {
     distanceFromEnd: number,
-    altitude: number
+    altitude: number,
+    predictedN1: number,
 }
 
 export class ClimbPathBuilder {
@@ -31,12 +32,14 @@ export class ClimbPathBuilder {
         checkpoints.push({
             distanceFromEnd: totalDistance - takeoffRollDistance,
             altitude: airfieldElevation,
+            predictedN1: SimVar.GetSimVarValue('L:A32NX_AUTOTHRUST_THRUST_LIMIT_TOGA', 'Percent'),
         });
 
         const { distanceTraveled: distanceTraveledSrs } = this.computeTakeoffStepPrediction(isaDev, airfieldElevation, accelerationAltitude, fmgc.getV2Speed(), zeroFuelWeight, fmgc.getFOB() * this.TONS_TO_POUNDS, tropoPause);
         checkpoints.push({
             distanceFromEnd: totalDistance - (takeoffRollDistance + distanceTraveledSrs),
             altitude: accelerationAltitude,
+            predictedN1: SimVar.GetSimVarValue('L:A32NX_AUTOTHRUST_THRUST_LIMIT_TOGA', 'Percent'),
         });
 
         const cruiseAltitude = SimVar.GetSimVarValue('L:AIRLINER_CRUISE_ALTITUDE', 'number');
@@ -46,14 +49,15 @@ export class ClimbPathBuilder {
 
         for (let altitude = accelerationAltitude; altitude < cruiseAltitude; altitude = Math.min(altitude + 1000, cruiseAltitude)) {
             const climbSpeed = altitude > 10000 ? fmgc.getManagedClimbSpeed() : 250;
-            const { distanceTraveled: distanceTraveledSegment, fuelBurned } = this.computeClimbSegmentPrediction(altitude, Math.min(altitude + 1000, cruiseAltitude), isaDev, climbSpeed, zeroFuelWeight, fob, tropoPause);
+            const { predictedN1: commandedN1, distanceTraveled: distanceTraveledSegment, fuelBurned } = this.computeClimbSegmentPrediction(altitude, Math.min(altitude + 1000, cruiseAltitude), isaDev, climbSpeed, zeroFuelWeight, fob, tropoPause);
 
             totalDistanceForClb += distanceTraveledSegment;
             fob -= fuelBurned;
 
             checkpoints.push({
                 distanceFromEnd: totalDistance - (totalDistanceForClb + takeoffRollDistance + distanceTraveledSrs),
-                altitude: Math.min(altitude + 1000, cruiseAltitude)
+                altitude: Math.min(altitude + 1000, cruiseAltitude),
+                predictedN1: commandedN1,
             })
         }
 
@@ -72,16 +76,16 @@ export class ClimbPathBuilder {
         }
     }
 
-    private static printAltitudePredictionsAtAltitudes(geometry: Geometry, checkpoints: VerticalCheckpoint[]): number {
+    private static printAltitudePredictionsAtAltitudes(geometry: Geometry, sortedCheckpoints: VerticalCheckpoint[]): number {
         let totalDistanceFromEnd = 0;
-        console.log(`checkpoints: ${JSON.stringify(checkpoints)}`);
+        console.log(sortedCheckpoints);
 
         for (const [i, leg] of geometry.legs.entries()) {
             totalDistanceFromEnd += leg.distance;
 
             if (leg instanceof TFLeg || leg instanceof RFLeg) {
-                const predictedAltitude = this.interpolateAltitude(totalDistanceFromEnd, checkpoints.map(checkpoint => checkpoint.distanceFromEnd), checkpoints.map(checkpoint => checkpoint.altitude));
-                console.log({ totalDistanceFromEnd, "waypoint": leg.from.ident, predictedAltitude })
+                const predictedAltitude = this.interpolateAltitude(totalDistanceFromEnd, sortedCheckpoints);
+                console.log({ totalDistanceFromEnd, "waypoint": leg.from.ident, predictedAltitude, constraint: leg.altitudeConstraint })
             } else {
                 console.warn(`[FMS/VNAV] Invalid leg when printing flightplan`)
             }
@@ -90,28 +94,28 @@ export class ClimbPathBuilder {
         return totalDistanceFromEnd;
     }
 
-    private static interpolateAltitude(x0: number, x: number[], y: number[]): number {
-        if (x0 > x[0]) {
-            return y[0]
+    private static interpolateAltitude(distanceFromEnd: number, sortedCheckpoints: VerticalCheckpoint[]): number {
+        if (distanceFromEnd > sortedCheckpoints[0].distanceFromEnd) {
+            return sortedCheckpoints[0].altitude;
         }
 
-        for (let index = 0; index < x.length - 1; index++) {
-            if (x0 <= x[index] && x0 > x[index + 1]) {
-                return y[index] + (x0 - x[index + 1]) * (y[index + 1] - y[index]) / (x[index] - x[index + 1])
+        for (let i = 0; i < sortedCheckpoints.length - 1; i++) {
+            if (distanceFromEnd <= sortedCheckpoints[i].distanceFromEnd && distanceFromEnd > sortedCheckpoints[i + 1].distanceFromEnd) {
+                return sortedCheckpoints[i + 1].altitude - (distanceFromEnd - sortedCheckpoints[i + 1].distanceFromEnd) * (sortedCheckpoints[i + 1].altitude - sortedCheckpoints[i].altitude) / (sortedCheckpoints[i].distanceFromEnd - sortedCheckpoints[i + 1].distanceFromEnd);
             }
         }
 
-        return y[y.length - 1]
+        return sortedCheckpoints[sortedCheckpoints.length - 1].altitude;
     }
 
-    private static computeTakeoffStepPrediction(isaDev: number, starting_altitude: number, accelerationAltitude: number, v2: number, zeroFuelWeight: number, fuelWeight: number, tropoPause: number): StepResults {
+    private static computeTakeoffStepPrediction(isaDev: number, starting_altitude: number, accelerationAltitude: number, v2: number, zeroFuelWeight: number, fuelWeight: number, tropoPause: number): StepResults & { predictedN1: number } {
         const midwayAltitudeSrs = (accelerationAltitude + starting_altitude) / 2;
 
-        const commandedN1Toga = SimVar.GetSimVarValue('L:A32NX_AUTOTHRUST_THRUST_LIMIT_TOGA', 'Percent');
+        const predictedN1 = SimVar.GetSimVarValue('L:A32NX_AUTOTHRUST_THRUST_LIMIT_TOGA', 'Percent');
 
         const machSrs = this.computeMachFromCas(midwayAltitudeSrs, isaDev, v2 + 10);
 
-        return Predictions.altitudeStep(starting_altitude, accelerationAltitude - starting_altitude, v2 + 10, machSrs, commandedN1Toga, zeroFuelWeight, fuelWeight, 0, isaDev, tropoPause, false, FlapConf.CONF_1);
+        return { predictedN1, ...Predictions.altitudeStep(starting_altitude, accelerationAltitude - starting_altitude, v2 + 10, machSrs, predictedN1, zeroFuelWeight, fuelWeight, 0, isaDev, tropoPause, false, FlapConf.CONF_1) };
     }
 
     private static printDistanceFromTocToClosestWaypoint(geometry: Geometry, distanceToTopOfClimbFromEnd: number) {
@@ -145,13 +149,14 @@ export class ClimbPathBuilder {
 
         return Common.CAStoMach(speed, deltaSrs);
     }
-    private static computeClimbSegmentPrediction(startingAltitude: number, targetAltitude: number, isaDev: number, climbSpeed: number, zeroFuelWeight: number, fob: number, tropoPause: number): StepResults {
+    private static computeClimbSegmentPrediction(startingAltitude: number, targetAltitude: number, isaDev: number, climbSpeed: number, zeroFuelWeight: number, fob: number, tropoPause: number): StepResults & { predictedN1: number } {
         const midwayAltitudeClimb = (startingAltitude + targetAltitude) / 2;
 
         const machClimb = this.computeMachFromCas(midwayAltitudeClimb, isaDev, climbSpeed);
         const estimatedTat = this.totalAirTemperatureFromMach(midwayAltitudeClimb, machClimb, isaDev)
-        const commandedN1Climb = this.getClimbThrustN1Limit(estimatedTat, midwayAltitudeClimb);
-        return Predictions.altitudeStep(startingAltitude, targetAltitude - startingAltitude, climbSpeed, machClimb, commandedN1Climb, zeroFuelWeight, fob, 0, isaDev, tropoPause);
+        const predictedN1 = this.getClimbThrustN1Limit(estimatedTat, midwayAltitudeClimb);
+
+        return { predictedN1 , ...Predictions.altitudeStep(startingAltitude, targetAltitude - startingAltitude, climbSpeed, machClimb, predictedN1, zeroFuelWeight, fob, 0, isaDev, tropoPause) };
     }
 
     private static computeTotalFlightPlanDistance(geometry: Geometry): number {
@@ -176,6 +181,6 @@ export class ClimbPathBuilder {
 
     static computeTakeOffRollDistance(): number {
         // TODO
-        return 1;
+        return 0.6;
     }
 }
